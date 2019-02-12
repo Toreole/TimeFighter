0d5d7ee4b9eb74e50ac6c53bcf7268dd969fbed1
Victor Fugazzotto <victorfuga@gmail.com>
2019-02-12 14:38:24 -0500

    Starting a controller
    Works as good as your local politicians

---
+++ Assets/_Scripts/Controller/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Input;
+
+namespace Controller
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private float _horizontalDir;
+        [SerializeField] private float moveSpeed = 3f;
+        
+        private Rigidbody2D _rigid;
+
+        private void Start()
+        {
+            _rigid = GetComponent<Rigidbody2D>();
+            _rigid.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+
+        private void Update()
+        {
+            _horizontalDir = Input.GetAxis("Horizontal");
+        }
+
+        private void FixedUpdate()
+        {
+            var moveDir = Vector2.right * _horizontalDir;
+            _rigid.velocity = moveDir * moveSpeed;
+        }
+    }
+}
