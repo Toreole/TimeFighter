e8916eeab346f65961428ed68a637b0fe4bc6cd7
Victor Fugazzotto <victorfuga@gmail.com>
2019-02-15 14:50:19 -0500

    Accidental delet
    Controller randomly gets stuck for some reason

---
+++ Assets/_Scripts/Controller/InputHandler.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Game.Controller
+{
+    // TODO: Switch to new input method?
+    public class InputHandler : MonoBehaviour
+    {
+        private float _horizontal;
+        private bool _jump;
+
+        private StateManager _states;
+        private float _delta;
+
+        private void Start()
+        {
+            _states = GetComponent<StateManager>();
+            _states.Init();
+        }
+
+        private void FixedUpdate()
+        {
+            _delta = Time.fixedDeltaTime;
+            UpdateStates();
+            _states.FixedTick(_delta);
+        }
+
+        private void Update()
+        {
+            GetInput();
+        }
+
+        private void GetInput()
+        {
+            _horizontal = Input.GetAxis("Horizontal");
+
+            _jump = Input.GetButtonDown("Jump");
+        }
+
+        private void UpdateStates()
+        {
+            _states.horizontal = _horizontal;
+            _states.moveAmount = Mathf.Clamp01(Mathf.Abs(_horizontal));
+            _states.jumpInput = _jump;
+        }
+    }
+
+}
