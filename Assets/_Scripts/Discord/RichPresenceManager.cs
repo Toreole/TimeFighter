ffe76312fb8500e687d4495520ba4cedc666f3df
Toreole <toreole.official@gmail.com>
2019-03-15 15:49:16 +0100

    imported Discord RPC

---
+++ Assets/_Scripts/Discord/RichPresenceManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using Discord;
+
+namespace Game
+{
+    public class RichPresenceManager : MonoBehaviour
+    {
+        [SerializeField]
+        protected long application_id;
+        [SerializeField]
+        DiscordRpc.RichPresence presence;
+    }
+}
