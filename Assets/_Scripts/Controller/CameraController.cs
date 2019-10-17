c5b17f046b772aaf1a6d242572a97f92b3434b7e
Toreole <toreole.official@gmail.com>
2019-10-17 15:06:39 +0200

    slowmo zones because its cool lol

--- Assets/_Scripts/Controller/CameraController.cs
+++ Assets/_Scripts/Controller/CameraController.cs
@@ -21,6 +21,10 @@ namespace Game.Controller
         protected Transform target;
         [SerializeField]
         protected float defaultSize = 9.5f;
+        [SerializeField]
+        protected AnimationCurve zoomCurve;
+
+        public static float DefaultSize => _instance.defaultSize;
 
         //set up all the stuff
         void Start()
@@ -32,7 +36,7 @@ namespace Game.Controller
             }
             if (!camera)
                 camera = GetComponent<Camera>();
-
+            _instance = this;
             camera.orthographicSize = defaultSize;
         }
 
@@ -44,6 +48,31 @@ namespace Game.Controller
             transform.position = new Vector3(pos.x, pos.y, transform.position.z);
         }
 
+        void M_DynamicZoom(float size, float time)
+        {
+            StopAllCoroutines(); //just to make sure
+            StartCoroutine(ZoomIn());
+            IEnumerator ZoomIn()
+            {
+                float startSize = camera.orthographicSize;
+                for(float t = 0; t < time; t += Time.deltaTime) //pretty standard stuff
+                {
+                    float tValue = zoomCurve.Evaluate(t / time);
+                    camera.orthographicSize = Mathf.Lerp(startSize, size, tValue);
+                    yield return null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Zooms the main camera to the given size over the time.
+        /// </summary>
+        /// <param name="size">the orthographic size to zoom to.</param>
+        /// <param name="time">the time that the zoom takes.</param>
+        public static void DynamicZoom(float size, float time = 0.4f)
+        {
+            _instance.M_DynamicZoom(size, time);
+        }
 
     }
 }
---
+++ Assets/_Scripts/Controller/SlomoTestTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using Game.Controller;
+
+namespace Game
+{
+    public class SlomoTestTrigger : MonoBehaviour
+    {
+        public float slowedTime = 0.3f;
+        public float zoomSize = 4f;
+        PlayerController player;
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.CompareTag("Player"))
+            {
+                //TODO: dynamic zoom in
+                player = collision.gameObject.GetComponentInParent<PlayerController>();
+                player.OnPressJump += OnJump;
+                Time.timeScale = slowedTime;
+                CameraController.DynamicZoom(zoomSize);
+            }
+        }
+
+        void OnJump()
+        {
+            player.OnPressJump -= OnJump;
+            //Debug.Log("Time is normal again");
+            Time.timeScale = 1;
+            CameraController.DynamicZoom(CameraController.DefaultSize); //now back out yall
+        }
+    }
+}
