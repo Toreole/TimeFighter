20b4fb139bbdc2d746a6e27669898169fdcd764e
Toreole <toreole.official@gmail.com>
2021-01-04 19:10:33 +0100

    Interaction for boss damage

---
+++ Assets/_Scripts/EnemyAI/DemoBoss/BossWeakSpot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Game.Interactions;
+
+namespace Game.Demo.Boss
+{
+    public class BossWeakSpot : Interactable
+    {
+        [SerializeField]
+        private BossController boss;
+        [SerializeField]
+        private float damagePerInteraction;
+        [SerializeField]
+        private float knockbackStrength;
+
+        public override void Interact(Game.Controller.Player entity)
+        {
+            boss.Damage(damagePerInteraction);
+            
+            Vector2 forceDirection = entity.Position - (Vector2)transform.position;
+            forceDirection.Normalize();
+            entity.Body.AddForce(forceDirection * knockbackStrength * entity.Body.mass, ForceMode2D.Impulse);
+        }
+    }
+}
